185be2a27e76c9e25795d625113faeef06408322
Josh Hartwig <joshman211@gmail.com>
2023-11-29 20:35:13 -0600

    working on day 7 solution for parsing fs

--- day_7/node/index.js
+++ day_7/node/index.js
@@ -1,58 +1,135 @@
 // read in sample.txt and print out the contents
-const fs = require('fs')
+const fs = require('fs');
+const { type } = require('os');
 const data = fs.readFileSync('sample.txt').toString().split('\n')
-
-
-/*
-iterate through each line of the file
-if $ is found, then process the command
- - if ls then process until next command is hit and add each item that is a a file to the object as a new object
-- fs contents can contain other objects. fs.contents.a.contents etc and a files array that contains {name: size:}
-
-*/
 
 const fileSystem = {
     contents: [],
-    get root() { 
+    _currentWorkingDirectory: null,
+    _prevWorkingDirectory: null,
+  
+    get currentWorkingDirectory() {
+      return this._currentWorkingDirectory || this.contents;
+    },
+  
+    set currentWorkingDirectory(val) {
+      console.log(`setting currentworking directory from ${JSON.stringify(this._currentWorkingDirectory,filterCircular,2)} to 
+        ${JSON.stringify(val,filterCircular,2)}`)
+      this._currentWorkingDirectory = val;
+    },
+  
+    get prevWorkingDirectory() {
+      return this._prevWorkingDirectory || this.contents;
+    },
+  
+    set prevWorkingDirectory(val) {
+      this._prevWorkingDirectory = val;
+    },
+
+    get root() {
         return this.contents
     },
-    currentWorkingDirectory: null,
-    prevWorkingDirectory: this.root
+
+    add: function (val) {
+        console.log(`attempting to add ${JSON.stringify(val,filterCircular,2)} to ${JSON.stringify(this.currentWorkingDirectory,filterCircular,2)}`)
+        this.currentWorkingDirectory.push(val)
+    }
+  };
+
+const filterCircular = (k,v) => {
+    if(k === 'parent') return 'circular'
+    return v
 }
+
 const parseInput = (input) => {
-    cmdStart = 0
-    cmdEnd = 0
-    
+
     for(let i = 0; i < input.length; i++) {
         if(input[i][0] === '$') {
             processCommand(input[i])
         }
         else {
-            // add each of the files or folders to the current working directory
+            if(input[i].includes('dir'))    // add a directory to current working directory
+            {
+                let dirname = input[i].substring(3,input[i].length).toString().trim()
+                fileSystem.add({
+                    name: dirname,
+                    contents: [],
+                    parent: fileSystem.currentWorkingDirectory
+                })
+            }
+            else {  // add files
+                let spaceIdx = input[i].indexOf(' ')
+                let size = input[i].substring(0,spaceIdx).toString().trim()
+                let filename = input[i].substring(spaceIdx,input[i].length).toString().trim()
+                fileSystem.add({
+                    name: filename,
+                    size: size,
+                })
+            }
         }
-        
-
     }
 }
 
+function printFileSystem(fs, indent = '') {
+    fs.contents.forEach(item => {
+        if (item.hasOwnProperty('contents')) {
+            // It's a directory
+            console.log(`${indent}- ${item.name} (dir)`);
+            printFileSystem(item, indent + '  ');
+        } else {
+            // It's a file
+            console.log(`${indent}- ${item.name} (file, size=${item.size})`);
+        }
+    });
+}
+
+// takes in contents array and returns total size
+function calculateDirSize(fs, parent) {
+    let size = 0
+    fs.contents.forEach(i => {
+        if(i.hasOwnProperty('size')) {
+            size += parseInt(i.size)
+        }
+        if(i.hasOwnProperty('contents')) {
+            calculateDirSize(i, i.name)
+        }
+    })
+    if(!parent) {
+        parent = "root"
+    }
+    console.log(`${parent} - ${size}`)
+}
+
+// if $ls i need to add everything between the commands to the current working directory
 const processCommand = (command) => {  
     if(command.includes('cd')) {
         let dir = command.slice(4).trim()
         if(dir === '/') { 
+            console.log(`changing cwd: ${JSON.stringify(fileSystem.currentWorkingDirectory,filterCircular,2)} to root`)
             fileSystem.currentWorkingDirectory = fileSystem.root
         }
         else if(dir === '..') {
+            let prevOfPrev
+            if(!fileSystem.prevWorkingDirectory.prevWorkingDirectory) {
+                prevOfPrev = fileSystem.prevWorkingDirectory.prevWorkingDirectory
+            }
             fileSystem.currentWorkingDirectory = fileSystem.prevWorkingDirectory
+            fileSystem.prevWorkingDirectory = prevOfPrev
         }
         else {
             // try and find the directory listed and change current working directory to it.
-            let tmp = fileSystem.currentWorkingDirectory?.contents?.find(obj => obj.name === dir) 
-            console.log(`tmp ${tmp} and dir ${dir}`)
+            let tmp = fileSystem.currentWorkingDirectory.find(obj => obj.name === dir) 
+            if(tmp) {
+                fileSystem.currentWorkingDirectory = tmp.contents;
+            } else {
+                console.log(`${dir} not found`)
+            }
         }
 
     }
 }
 
-fileSystem.contents.push({name:'d', contents:[], parent: fileSystem.currentWorkingDirectory})
 parseInput(data)
+printFileSystem(fileSystem)
+calculateDirSize(fileSystem)
 
